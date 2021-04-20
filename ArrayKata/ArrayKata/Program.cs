908b2f10cdf32bf26b90197ba0239b1d0946efec
Pavel Berezkin <Berezkin.P@dns-shop.ru>
2021-04-20 11:44:54 +1000

    Ката с поиском слова "needle" в массиве объектов

---
+++ ArrayKata/ArrayKata/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Linq;
+
+namespace ArrayKata
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(FindNeedle(new object[] { 1, null, "asdasd", "needle" }));
+            Console.WriteLine(FindNeedleV2(new object[] { 1, null, "asdasd", "needle" }));
+            Console.WriteLine(FindNeedleV3(new object[] { 1, null, "asdasd", "needle" }));
+            
+            Console.ReadLine();
+        }
+
+        #region FindNeedle
+
+        public static string FindNeedle(object[] haystack)
+        {
+            int indexNeedle = Array.IndexOf(haystack, "needle");
+            return $"found the needle at position {indexNeedle}";
+        }
+
+        public static string FindNeedleV2(object[] haystack)
+        {
+            int i = 0;
+            while (!ReferenceEquals(haystack[i], "needle"))
+            {
+                i++;
+            }
+
+            return $"found the needle at position {i}";
+        }
+
+        public static string FindNeedleV3(object[] haystack)
+        {
+            return $"found the needle at postion {haystack.ToList().IndexOf("needle")}";
+        }
+
+        #endregion
+    }
+}
